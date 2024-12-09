5176f7b66563a1d2e7a9d09ce729aea8b6430c87
Fabricio <fabriciocolombo0096@gmail.com>
2024-12-09 14:15:14 -0300

    add spring boot backend

---
+++ backend/src/main/java/com/anasemijoias/catalogo_semi_joias/CatalogoSemiJoiasApplication.java
@@ -0,0 +1,13 @@
+package com.anasemijoias.catalogo_semi_joias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoSemiJoiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoSemiJoiasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/anasemijoias/catalogo_semi_joias/CatalogoSemiJoiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasemijoias.catalogo_semi_joias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoSemiJoiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
